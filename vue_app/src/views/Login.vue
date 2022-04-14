05e2b897ee0627fccf146f229fce056ec211c0cc
liulei <1218362859@qq.com>
2022-04-14 09:28:34 +0800

    修改首页事件

--- vue_app/src/views/Login.vue
+++ vue_app/src/views/Login.vue
@@ -1,46 +1,40 @@
 import Vue from 'vue'
-<template id="login_page">
-  <van-button type="info" class="van-button" 
-    >进入</van-button
-  >
+<template>
+  <div class="login_box">
+    <van-button
+      type="info"
+      class="van_button"
+      @click="initDBHelper('123213wdssds')"
+      >进入</van-button
+    >
+  </div>
 </template>
 
 <script>
-
 export default {
   name: "Login",
+  data: {},
+  mounted() {
+    alert("123");
+  },
+  methods: {
+    initDBHelper: function (message) {
+      alert(message);
+      cordova.plugins.DBHelper.coolMethod("hello", onSuccess(), onError());
+    },
+    onSuccess: function () {},
+    onerror: function () {},
+  },
 };
-// var app = new Vue({
-//   // element的缩写，挂载元素
-//   el: "#login_page",
-//   data: {},
-//   mounted() {
-//           alert("123");
-//   },
-//   methods: {
-    
-//     initDBHelper: function () {
-//       alert("123");
-//       cordova.plugins.DBHelper.coolMethod("hello", onSuccess, onError);
-//     },
-//     onSuccess: function () {},
-//     onerror: function () {},
-//   },
-// });
 </script>
 
 <style lang="css" scoped>
-.login_box {
-  text-align: center;
-  background-color: blue;
-}
-.van-button {
+
+.van_button {
   display: block;
   margin: 0 auto;
-  margin-top: 200px;
+  margin-top: 20rem;
   background-color: chocolate;
-}
-#login_page {
-  background: blue;
+  border: 0rem;
 }
 </style>
